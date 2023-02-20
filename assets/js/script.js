794bcf978a4ba48032539bd5a240f12d0d5d1573
roberto-vera <robertodevstack@gmail.com>
2023-02-20 18:38:42 -0300

    commit1

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+    faq.addEventListener("click", () => {
+        faq.classList.toggle("active");
+    });
+});
+
+
+ const filterContainer = document.querySelector(".content-filter"),
+ galleryItems = document.querySelectorAll(".gallery-item");
+
+ filterContainer.addEventListener("click", (event) =>{
+   if(event.target.classList.contains("filter-item")){
+   	 filterContainer.querySelector(".active").classList.remove("active");
+   	 event.target.classList.add("active");
+   	 const filterValue = event.target.getAttribute("data-filter");
+   	 galleryItems.forEach((item) =>{
+       if(item.classList.contains(filterValue) || filterValue === 'all'){
+       	item.classList.remove("hide");
+       	 item.classList.add("show");
+       }
+       else{
+       	item.classList.remove("show");
+       	item.classList.add("hide");
+       }
+   	 });
+   }
+ });
